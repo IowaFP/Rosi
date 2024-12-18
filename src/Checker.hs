f7fcb44d5adef63d32f64a8144d25c60624c0c17
J. Garrett Morris <garrett-morris@uiowa.edu>
2024-12-18 16:48:05 -0600

    Normalize types after kind checking.

--- src/Checker.hs
+++ src/Checker.hs
@@ -1,4 +1,4 @@
-module Checker (runCheckM, runCheckM', checkTy, checkTop, TCtxt(..), traceTypeInference) where
+module Checker (runCheckM, runCheckM', checkTy, checkTop, normalize, TCtxt(..), traceTypeInference) where
 
 import Control.Monad.Except
 import Control.Monad.Reader
