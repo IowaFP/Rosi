12692a4a0d4a83a78176f7f83da906ebcce2b704
J. Garrett Morris <garrett-morris@uiowa.edu>
2024-12-19 16:05:21 -0600

    Refine implicit introduction of `/\`s.
    Now, *in the primary subderivations of an explicit type annotation*
     we do not introduce implicit `/\`s.  You get implicit `/\`s back in
     secondary subderivations (e.g., arguments to a function).
    
     This is what you might call an ad hoc, and also fragile, solution, but
     it allows us to get around the cases where we *need* to specify a
     polymorphic type for an expression to get the predicates aligned right.

--- src/Checker/Terms.hs
+++ src/Checker/Terms.hs
@@ -30,13 +30,15 @@ wrap q t = case flattenQ q of
              QRefl -> t
              q'    -> ETyEqu t q'
 
-checkTerm' :: Term -> Ty -> CheckM Term
-checkTerm' e t = checkTerm e =<< flattenT t
+checkTerm' :: Bool -> Term -> Ty -> CheckM Term
+checkTerm' implicitTyLams e t = checkTerm implicitTyLams e =<< flattenT t
 
 lookupVar :: Int -> CheckM Ty
 lookupVar i = asks (lookupV i . tctxt)
 
 inst :: Term -> Ty -> Ty -> CheckM Term
+inst e actual@(TForall {}) expected@(TForall {}) = -- Could have a little finer resolution here: suppose the actual type has *more* quantifiers than the expected type?
+    flip wrap e <$> expectT e actual expected
 inst e (TForall x k t) expected =
   do u <- typeGoal' "t" k
      t' <- shiftTN 0 (-1) <$> subst 0 (shiftTN 0 1 u) t
@@ -49,39 +51,39 @@ inst e (TThen p t) expected =
      inst (EPrApp e (VGoal (Goal ("v", vr)))) t expected
 inst e t expected = flip wrap e <$> expectT e t expected
 
-checkTerm :: Term -> Ty -> CheckM Term
-checkTerm m t = 
+checkTerm :: Bool -> Term -> Ty -> CheckM Term
+checkTerm implicitTyLams m t = 
   do g <- asks tctxt
      trace $ "checkTerm (" ++ show m ++ ") (" ++ show t ++ ") in (" ++ show g ++ ")"
-     checkTerm0 m t
+     checkTerm0 implicitTyLams m t
 
-checkTerm0 :: Term -> Ty -> CheckM Term
-checkTerm0 e0@(ETyLam v k e) expected =
+checkTerm0 :: Bool -> Term -> Ty -> CheckM Term
+checkTerm0 implicitTyLams e0@(ETyLam v k e) expected =
   do tcod <- typeGoal "cod"
      q <- expectT e0 (TForall v k tcod) expected
-     wrap q . ETyLam v k <$> bindTy k (checkTerm' e tcod)
-checkTerm0 e (TForall v k t) =
-  ETyLam v k <$> bindTy k (checkTerm e t)
-checkTerm0 e0@(EPrLam p e) expected =
+     wrap q . ETyLam v k <$> bindTy k (checkTerm' implicitTyLams e tcod) -- is this the right call for implicits? maybe once you've gone explicit, you should have to stay explicit....?
+checkTerm0 True e (TForall v k t) =
+  ETyLam v k <$> bindTy k (checkTerm True e t)
+checkTerm0 implicitTyLams e0@(EPrLam p e) expected =
   do tcod <- typeGoal "cod"
      q <- expectT e0 (TThen p tcod) expected
-     wrap q . EPrLam p <$> assume p (checkTerm' e tcod)
-checkTerm0 e (TThen p t) =
-  EPrLam p <$> assume p (checkTerm e t)
-checkTerm0 (EVar (-1) x) expected =   
+     wrap q . EPrLam p <$> assume p (checkTerm' implicitTyLams e tcod)
+checkTerm0 implicitTyLams e (TThen p t) =
+  EPrLam p <$> assume p (checkTerm implicitTyLams e t)
+checkTerm0 implicitTyLams (EVar (-1) x) expected =   
   throwError (ErrOther $ "scoping error: variable " ++ x ++ " not resolved")
-checkTerm0 e@(EVar i v) expected = flip (inst (EVar i v)) expected =<< flattenT =<< lookupVar i -- . unshift =<< lookupVar i
-checkTerm0 e0@(ELam v t e) expected =
+checkTerm0 implicitTyLams e@(EVar i v) expected = flip (inst (EVar i v)) expected =<< flattenT =<< lookupVar i -- . unshift =<< lookupVar i
+checkTerm0 implicitTyLams e0@(ELam v t e) expected =
   do tcod <- typeGoal "cod"
-     t' <- checkTy' e0 t KType
+     t' <- fst <$> (normalize' =<< checkTy' e0 t KType)
      q <- expectT e0 (funTy t' tcod) expected
-     wrap q . ELam v t' <$> bind t' (checkTerm' e tcod)
-checkTerm0 e0@(EApp f e) expected =
+     wrap q . ELam v t' <$> bind t' (checkTerm' implicitTyLams  e tcod)
+checkTerm0 implicitTyLams e0@(EApp f e) expected =
   do tdom <- typeGoal "dom"
      EApp <$>
-       checkTerm f (funTy tdom expected) <*>
-       checkTerm' e tdom
-checkTerm0 e0@(ETyApp e t) expected
+       checkTerm implicitTyLams f (funTy tdom expected) <*>
+       checkTerm' implicitTyLams  e tdom
+checkTerm0 implicitTyLams e0@(ETyApp e t) expected
   | Just ((i, x), ts) <- insts e0 =
     do u <- flattenT =<< lookupVar i
        let (ks, actual) = quants u
@@ -95,7 +97,7 @@ checkTerm0 e0@(ETyApp e t) expected
   | otherwise =
     -- Saddest of faces...
     do et <- typeGoal "t"
-       e' <- checkTerm e et
+       e' <- checkTerm implicitTyLams e et
        et' <- flattenT et
        case et' of
          TForall x k u ->
@@ -103,94 +105,94 @@ checkTerm0 e0@(ETyApp e t) expected
               q <- expectT e0 u' expected
               return (wrap q (ETyApp e' t))
          _ -> fail $ "in " ++ show e0 ++ ": expected " ++ show et' ++ " to be a quantified type"
-checkTerm0 e0@(EPrApp e p) expected =
+checkTerm0 implicitTyLams e0@(EPrApp e p) expected =
   fail "unimplemented"
-checkTerm0 e0@(ESing t) expected =
+checkTerm0 implicitTyLams e0@(ESing t) expected =
   do q <- expectT e0 (TSing t) expected
      wrap q . ESing <$> checkTy' e0 t KLabel
-checkTerm0 e0@(ELabel el e) expected =
+checkTerm0 implicitTyLams e0@(ELabel el e) expected =
   do tl <- typeGoal' "l" KLabel
      t <- typeGoal "t"
      q <- expectT e0 (TLabeled tl t) expected
      wrap q <$>
-       (ELabel <$> checkTerm' el (TSing tl) <*> checkTerm' e t)
-checkTerm0 e0@(EUnlabel e el) expected =
+       (ELabel <$> checkTerm' implicitTyLams  el (TSing tl) <*> checkTerm' implicitTyLams  e t)
+checkTerm0 implicitTyLams e0@(EUnlabel e el) expected =
   do tl <- typeGoal' "l" KLabel
-     el' <- checkTerm el (TSing tl)
-     e' <- checkTerm' e (TLabeled tl expected)
+     el' <- checkTerm implicitTyLams el (TSing tl)
+     e' <- checkTerm' implicitTyLams  e (TLabeled tl expected)
      return (EUnlabel e' el')
-checkTerm0 e0@(EPrj y z v@(VGoal (Goal (_, r))) e) expected =
+checkTerm0 implicitTyLams e0@(EPrj y z v@(VGoal (Goal (_, r))) e) expected =
   do y' <- checkTy y (KRow (kindOf expected))
      z' <- checkTy z (KRow (kindOf expected))
      q <- expectT e0 (TPi y') expected
-     e' <- checkTerm e (TPi z')
+     e' <- checkTerm implicitTyLams e (TPi z')
      require (PLeq y' z') r
      return (wrap q (EPrj y' z' v e'))  -- but let's go ahead and flatten this term...
-checkTerm0 (EPrj {}) _ =
+checkTerm0 implicitTyLams (EPrj {}) _ =
   fail "unimplemented: prj with non-goal evidence"
-checkTerm0 e0@(EConcat x y z v@(VGoal (Goal (_, r))) m n) expected =
+checkTerm0 implicitTyLams e0@(EConcat x y z v@(VGoal (Goal (_, r))) m n) expected =
   do x' <- checkTy x (KRow (kindOf expected))
      y' <- checkTy y (KRow (kindOf expected))
      z' <- checkTy z (KRow (kindOf expected))
      q <- expectT e0 (TPi z') expected
-     m' <- checkTerm m (TPi x')
-     n' <- checkTerm n (TPi y')
+     m' <- checkTerm implicitTyLams m (TPi x')
+     n' <- checkTerm implicitTyLams n (TPi y')
      require (PPlus x' y' z') r
      return (wrap q (EConcat x' y' z' v m' n'))
-checkTerm0 (EConcat {}) _ =
+checkTerm0 implicitTyLams (EConcat {}) _ =
   fail "unimplemented: ++ with non-goal evidence"
-checkTerm0 e0@(EInj y z v@(VGoal (Goal (_, r))) e) expected =
+checkTerm0 implicitTyLams e0@(EInj y z v@(VGoal (Goal (_, r))) e) expected =
   do y' <- checkTy y (KRow (kindOf expected))
      z' <- checkTy z (KRow (kindOf expected))
      q <- expectT e0 (TSigma z') expected
-     e' <- checkTerm e (TSigma y')
+     e' <- checkTerm implicitTyLams e (TSigma y')
      require (PLeq y' z') r
      return (wrap q (EPrj y' z' v e'))  -- but let's go ahead and flatten this term...
-checkTerm0 (EInj {}) _ =
+checkTerm0 implicitTyLams (EInj {}) _ =
   fail "unimplemented: inj with non-goal evidence"
-checkTerm0 e0@(EBranch x y z v@(VGoal (Goal (_, r))) m n) expected =
+checkTerm0 implicitTyLams e0@(EBranch x y z v@(VGoal (Goal (_, r))) m n) expected =
   do x' <- checkTy x (KRow (kindOf expected))
      y' <- checkTy y (KRow (kindOf expected))
      z' <- checkTy z (KRow (kindOf expected))
      t <- typeGoal "t"
      q <- expectT e0 (funTy (TSigma z') t) expected
      t' <- flattenT t
-     m' <- checkTerm m (funTy (TSigma x') t')
-     n' <- checkTerm n (funTy (TSigma y') t')
+     m' <- checkTerm implicitTyLams m (funTy (TSigma x') t')
+     n' <- checkTerm implicitTyLams n (funTy (TSigma y') t')
      require (PPlus x' y' z') r
      return (wrap q (EBranch x' y' z' v m' n'))
-checkTerm0 (EBranch {}) _ =
+checkTerm0 implicitTyLams (EBranch {}) _ =
   fail "unimplemented: ++ with non-goal evidence"
-checkTerm0 e0@(EAna phi e) expected =
+checkTerm0 implicitTyLams e0@(EAna phi e) expected =
   do k <- kindGoal "k"
      phi' <- checkTy' e0 phi (KFun k KType)
      r <- typeGoal' "r" (KRow k)
      t <- typeGoal "t"
      q <- expectT e0 (TSigma (TApp (TMapFun phi') r) `funTy` t) expected
-     EAna phi' <$> checkTerm e (TForall "l" KLabel $ TForall "u" k $ TForall "y1" (KRow k) $ TForall "z" (KRow k) $ TForall "y2" (KRow k) $
+     EAna phi' <$> checkTerm implicitTyLams e (TForall "l" KLabel $ TForall "u" k $ TForall "y1" (KRow k) $ TForall "z" (KRow k) $ TForall "y2" (KRow k) $
                                 PPlus (TVar 2 "y1" (Just (KRow k))) (TRow [TLabeled (TVar 4 "l" (Just KLabel)) (TVar 3 "u" (Just k))]) (TVar 1 "z" (Just (KRow k))) `TThen`
                                 PPlus (TVar 1 "z" (Just (KRow k))) (TVar 0 "y2" (Just (KRow k))) (shiftTN 0 5 r) `TThen`
                                 TSing (TVar 4 "l" (Just KLabel)) `funTy` TApp (shiftTN 0 5 phi') (TVar 3 "u" (Just k)) `funTy` shiftTN 0 5 t)
-checkTerm0 e0@(ESyn phi e) expected =
+checkTerm0 implicitTyLams e0@(ESyn phi e) expected =
   do k <- kindGoal "k"
      phi' <- checkTy' e0 phi (KFun k KType)
      r <- typeGoal' "r" (KRow k)
      q <- expectT e0 (TPi (TApp (TMapFun phi') r)) expected
-     ESyn phi' <$> checkTerm e (TForall "l" KLabel $ TForall "u" k $ TForall "y1" (KRow k) $ TForall "z" (KRow k) $ TForall "y2" (KRow k) $
+     ESyn phi' <$> checkTerm implicitTyLams e (TForall "l" KLabel $ TForall "u" k $ TForall "y1" (KRow k) $ TForall "z" (KRow k) $ TForall "y2" (KRow k) $
                                 PPlus (TVar 2 "y1" (Just (KRow k))) (TRow [TLabeled (TVar 4 "l" (Just KLabel)) (TVar 3 "u" (Just k))]) (TVar 1 "z" (Just (KRow k))) `TThen`
                                 PPlus (TVar 1 "z" (Just (KRow k))) (TVar 0 "y2" (Just (KRow k))) r `TThen`
                                 TSing (TVar 4 "l" (Just KLabel)) `funTy` TApp (shiftTN 0 4 phi') (TVar 3 "u" (Just k)))
-checkTerm0 e0@(ETyped e t) expected =
+checkTerm0 implicitTyLams e0@(ETyped e t) expected =
   do (t', _) <- normalize =<< checkTy' e0 t KType
-     e' <- checkTerm e t'  -- any reason to preserve the type ascription?
+     e' <- checkTerm False e t'  -- any reason to preserve the type ascription?
      inst e0 t' expected
      
 
 
 checkTop :: Term -> Ty -> CheckM Term
 checkTop m t = 
-  do (t', q) <- normalize t
-     m' <- checkTerm m t'
+  do (t', q) <- normalize' t
+     m' <- checkTerm True m t'
      return (case q of
                QRefl -> m'
                _ -> ETyEqu m' q)
