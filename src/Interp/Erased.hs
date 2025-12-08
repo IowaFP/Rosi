5dc51b2a259d2550a6794b5f961411ecfe501e8f
J. Garrett Morris <garrett-morris@uiowa.edu>
2025-12-08 10:54:41 -0600

    Track boundedness of evidence.
    To see the essence of what's going on here, consider a projection
    
        prj [z - x, z, VComplLeq v] (every True)
    
    for some evidence variable `v`. Without knowing the size of of `z`,
    we have no idea of the size of the resulting record. We know which
    fields *not* to include, but there may be arbitrarily many fields.
    
    In the past, I basically ignored this issue entirely. As a result, the
    projection above would generate an arbitrarily large record, which
    could later cause divergence.
    
    I am still using "unbounded" evidence. I think there is a version that
    uses only finite evidence, annotated either as inclusion or exclusion
    evidence, but I am leaving that for a later effort.
    
    Fixes #11

--- src/Interp/Erased.hs
+++ src/Interp/Erased.hs
@@ -40,7 +40,18 @@ instance Show Value where
   show (VSyn t) = "<<syn>>"
   show (VString s) = "\"" ++ s ++ "\""
 
-data EValue = VLeq [Int] | VPlus [Either Int Int] | VEq | VVFold Int
+data EList t = Bounded [t] | Unbounded [t]
+  deriving (Show, Foldable, Functor, Traversable)
+
+(<!>) :: EList t -> Int -> t
+Bounded xs <!> n = xs !! n
+Unbounded xs <!> n = xs !! n
+
+listFrom :: EList t -> (Bool, [t])
+listFrom (Bounded xs) = (True,  xs)
+listFrom (Unbounded xs) = (False, xs)
+
+data EValue = VLeq (EList Int) | VPlus (EList (Either Int Int)) | VEq | VVFold Int
   deriving Show
 
 evalB :: Env -> Body -> Value
@@ -103,10 +114,14 @@ eval' h (EConst CPrj) = -- VPrLam h (Value (VLam h (Const CPrj)))
   VPrLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
     case h of
-      (VLeq is : _, v : _) ->
-        case v of
-          VSyn f -> VSyn (\i -> f (is !! i))
-          _ -> VRecord (map (recordFrom v) is)
+      (VLeq (Bounded is) : _, VSyn f : _) ->
+        VRecord (map f is)
+      (VLeq (Unbounded is) : _, VSyn f : _) ->
+        VSyn (\i -> f (is !! i))
+      (VLeq (Bounded is) : _, VRecord vs : _) ->
+        VRecord (map (vs !!) is)
+      (VLeq (Unbounded is) : _, VRecord vs : _) ->
+        VRecord [vs !! j | j <- takeWhile (< length vs) is]
       _ -> error $ "bad environment for prj: " ++ show h
 eval' h (EConst CInj) = -- VPrLam h (Value (VLam h (Const CPrj)))
   VPrLam h $ Prim $ \ h ->
@@ -114,88 +129,79 @@ eval' h (EConst CInj) = -- VPrLam h (Value (VLam h (Const CPrj)))
     case h of
       (VLeq is : _, v : _) ->
         let (k, w) = variantFrom v in
-        VVariant (is !! k) w
+        VVariant (is <!> k) w
       _ -> error $ "bad environment for inj: " ++ show h
 eval' h (EConst CConcat) = -- VPrLam h (Value (VLam h (Value (VLam h (Const CConcat)))))
   VPrLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \ (VPlus is : _, w : v : _) ->
-    case (v, w) of
-      (VSyn f, w) ->
-        let ws = recordFrom w
-            pick (Left i) = f i
-            pick (Right i) = ws i in
-        VSyn (\i -> pick (is !! i))
-      (v, VSyn g) ->
-        let vs = recordFrom v
-            pick (Left i) = vs i
-            pick (Right i) = g i in
-        VSyn (\i -> pick (is !! i))
-      _ ->
-        let ws = recordFrom w
-            vs = recordFrom v
-            pick (Left i) = vs i
-            pick (Right i) = ws i in
-        VRecord [pick (is !! i) | i <- [0..recordSize v + recordSize w - 1]]
+  VLam h $ Prim $ \case
+    (VPlus (Bounded is) : _, w : v : _) ->
+      let ws = recordFrom w
+          vs = recordFrom v
+          pick (Left i) = vs i
+          pick (Right j) = ws j in
+      VRecord [pick i | i <- is]
+    (VPlus (Unbounded is) : _, VRecord ws : VRecord vs : _) ->
+      let pick (Left i) = vs !! i
+          pick (Right i) = ws !! i in
+      VRecord [pick (is !! i) | i <- [0..length vs + length ws - 1]]
+    (VPlus (Unbounded is) : _, w : v : _) ->
+      let vs = recordFrom v
+          ws = recordFrom w
+          pick (Left i) = vs i
+          pick (Right i) = ws i in
+      VSyn (\i -> pick (is !! i))
 eval' h (EConst CBranch) = -- VPrLam h (Value (VLam h (Value (VLam h (Value (VLam h (Const CBranch)))))))
   VPrLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \h ->
-    case h of
-      (VPlus is : _, v : g : f : _) ->
-        let (k, w) = variantFrom v in
-        trace ("branch: constructor is " ++ show k ++
-               " and evidence is " ++ show is ++ " so calling " ++
-               (case is !! k of Left _ -> show f; Right _ -> show g)) $
-        case is !! k of
-          Left i  -> app f (VVariant i w)
-          Right i -> app g (VVariant i w)
-      _ -> error $ "bad environment for branch: " ++ show h
+  VLam h $ Prim $ \case
+    (VPlus is : _, v : g : f : _) ->
+      let (k, w) = variantFrom v in
+      trace ("branch: constructor is " ++ show k ++
+              " and evidence is " ++ show is ++ " so calling " ++
+              (case is <!> k of Left _ -> show f; Right _ -> show g)) $
+      case is <!> k of
+        Left i  -> app f (VVariant i w)
+        Right i -> app g (VVariant i w)
+    _ -> error $ "bad environment for branch: " ++ show h
 eval' h (EConst CIn) = -- VLam h (Const CIn)
   VLam h $ Prim $ \ (_, v : _) -> VIn v
 eval' h (EConst COut) = -- VLam h (Const COut)
-  VLam h $ Prim $ \ h ->
-    case h of
-      (_, VIn v : _) -> v
-      _ -> error $ "bad environment for out: " ++ show h
+  VLam h $ Prim $ \case
+    (_, VIn v : _) -> v
+    _ -> error $ "bad environment for out: " ++ show h
 eval' h (EConst CFix) = -- VLam h (Const CFix)
   eval h (ELam "f" Nothing (EApp (EVar 0 ["f", ""]) (EApp (EConst CFix) (EVar 0 ["f", []]))))
---   VLam h $ Prim $ \ (_, VLam (hp, he) (Term e) : _) ->
---     eval (hp, eval h (EApp (EConst CFix) (ELam "x" Nothing e)) : he) e
 eval' h (EConst CStringCat) =
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \h ->
-    case h of
-      (_, VString s1 : VString s0 : _) ->
-         VString (s0 ++ s1)
-      _ -> error $ "bad environment for (^): " ++ show h
+  VLam h $ Prim $ \case
+    (_, VString s1 : VString s0 : _) ->
+       VString (s0 ++ s1)
+    _ -> error $ "bad environment for (^): " ++ show h
 eval' h (EConst CSyn) =
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \h ->
-    case h of
-      (_, f : _) ->
-        VSyn (\i -> app (prapp f (VLeq [i])) VSing)
+  VLam h $ Prim $ \case
+    (_, f : _) ->
+      VSyn (\i -> app (prapp f (VLeq (Bounded [i]))) VSing)
 eval' h (EConst CAna) =
   VLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \h ->
-    case h of
-      (_, VVariant k w : f : _) ->
-        app (app (prapp f (VLeq [k])) VSing) w
-      (_, v : e : _) ->
-        error $ "bad argument for (ana" ++ show e ++ "): " ++ show v
+  VLam h $ Prim $ \case
+    (_, VVariant k w : f : _) ->
+      app (app (prapp f (VLeq (Bounded [k]))) VSing) w
+    (_, v : e : _) ->
+      error $ "bad argument for (ana" ++ show e ++ "): " ++ show v
 eval' h (EConst CFold) =
   VPrLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
   VLam h $ Prim $ \h ->
-  VLam h $ Prim $ \h ->
-    case h of
-      (VVFold n : _, r : def : comp : single : _) ->
-        let vs = recordFrom r
-            one k = app (app (prapp single (VLeq [k])) VSing) (vs k)
-        in if n == 0 then def else foldl (\v w -> app (app comp v) w) (one 0) (map one [1..n - 1])
+  VLam h $ Prim $ \case
+    (VVFold n : _, r : def : comp : single : _) ->
+      let vs = recordFrom r
+          one k = app (app (prapp single (VLeq $ Bounded [k])) VSing) (vs k)
+      in if n == 0 then def else foldl (\v w -> app (app comp v) w) (one 0) (map one [1..n - 1])
 eval' h (ECast e q) = q `seq` eval h e
 eval' h (ETyped e _) = eval h e
 eval' h (EStringLit s) = VString s
@@ -220,47 +226,54 @@ evalV h v@(VPlusComplR {}) = VPlus $ evalPlus h v
 evalV h (VFold n)          = VVFold n
 evalV h v                  = VEq
 
-evalLeq :: Env -> Evid -> [Int]
+evalLeq :: Env -> Evid -> EList Int
 evalLeq h@(hp, _) = go False where
 
-  go compl (VVar i) = go compl (VLeqSimple is) where
+  go :: Bool -> Evid -> EList Int
+  go False (VVar i) = is where
     VLeq is = hp !! i
-  go False VLeqRefl = [0..]
-  go True VLeqRefl = []
+  go True (VVar i) = Unbounded $ filter (`notElem` is) [0..] where
+    VLeq is = hp !! i
+  go False VLeqRefl = Unbounded [0..]
+  go True VLeqRefl = Bounded []
   go compl (VLeqTrans q1 q2)
-    | compl = filter (`notElem` ks) [0..]
+    | compl = Unbounded $ filter (`notElem` ks) [0..]
     | otherwise = ks
     where is = go False q1
           js = go False q2
-          ks = map (js !!) is
-  go False (VLeqSimple is) = is
-  go True (VLeqSimple is) = filter (`notElem` is) [0..]
+          ks = fmap (js <!>) is
+  go False (VLeqSimple is) = Bounded is
+  go True (VLeqSimple is) = Unbounded $ filter (`notElem` is) [0..]
   go compl (VLeqLiftL _ q) = go compl q
   go compl (VLeqLiftR q _) = go compl q
-  go compl (VPlusLeqL q) = map snd (sortOn fst [(i, j) | (j, Left i) <- zip [0..] es])
-    where es = evalPlus h q
-  go compl (VPlusLeqR q) = map snd (sortOn fst [(i, j) | (j, Right i) <- zip [0..] es])
-    where es = evalPlus h q
+  go compl (VPlusLeqL q) = k $ map snd (sortOn fst [(i, j) | (j, Left i) <- zip [0..] es])
+    where (esBounded, es) = listFrom $ evalPlus h q
+          k | esBounded = Bounded
+            | otherwise = Unbounded
+  go compl (VPlusLeqR q) = k $ map snd (sortOn fst [(i, j) | (j, Right i) <- zip [0..] es])
+    where (esBounded, es) = listFrom $ evalPlus h q
+          k | esBounded = Bounded
+            | otherwise = Unbounded
   go compl (VComplLeq q) = go (not compl) q
   go compl v = error $ "bad evidence for leq: " ++ show v
 
-evalPlus :: Env -> Evid -> [Either Int Int]
+evalPlus :: Env -> Evid -> EList (Either Int Int)
 evalPlus h v = evalPlus' h v
 
-
+evalPlus' :: Env -> Evid -> EList (Either Int Int)
 evalPlus' (hp, _) (VVar i) = es
   where VPlus es = hp !! i
-evalPlus' h (VPlusSimple es) = es
+evalPlus' h (VPlusSimple es) = Bounded $ es
 evalPlus' h (VPlusLiftL _ q) = evalPlus' h q
 evalPlus' h (VPlusLiftR q _) = evalPlus' h q
-evalPlus' h (VPlusComplL q) = map pick [0..]
+evalPlus' h (VPlusComplL q) = Unbounded $ map pick [0..]
   where is = evalLeq h q
-        pick i = case elemIndex i is of
+        pick i = case elemIndex i (snd $ listFrom is) of
                    Just j -> Right j
-                   Nothing -> Left (i - length [j | j <- is, j < i])
-evalPlus' h (VPlusComplR q) = map pick [0..]
+                   Nothing -> Left (i - length [j | j <- snd (listFrom is), j < i])
+evalPlus' h (VPlusComplR q) = Unbounded $ map pick [0..]
   where is = evalLeq h q
-        pick i = case elemIndex i is of
+        pick i = case elemIndex i (snd $ listFrom is) of
                    Just j -> Left j
-                   Nothing -> Right (i - length [j | j <- is, j < i])
+                   Nothing -> Right (i - length [j | j <- snd (listFrom is), j < i])
 evalPlus' _ v = error $ "bad evidence for plus: " ++ show v
